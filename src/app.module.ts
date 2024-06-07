26ad5f97fedfec4ae398cffd35e8ca415d977841
Emmah-oo <emmaochonogor715@gmail.com>
2024-06-07 16:45:06 +0100

    feat: added user controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
+import { User } from './user/entities/user.entity';
 
 @Module({
   imports: [
@@ -12,7 +13,7 @@ import { UserModule } from './user/user.module';
       port: 5432,
       password: '178946',
       username: 'postgres',
-      entities: [],
+      entities: [User],
       database: 'pgWithNest',
       synchronize: true,
       logging: true,
